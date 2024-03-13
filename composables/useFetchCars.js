a2271bc3a5334f51143f7b29d0cfaeb54e3fd3b6
Ashley Shang <utshang216@gmail.com>
2024-03-13 14:35:15 +0800

    所有產品頁：修改 資料渲染

--- composables/useFetchCars.js
+++ composables/useFetchCars.js
@@ -2,26 +2,11 @@ export default async (city, filters) => {
   // city 由參數取得
   // 可能會有其他 params 像是 minPrice 等等
   // 非同步 async await
-  // const { data, error, refresh } = await useFetch(`/api/cars/${city}`, {
-  //   params: {
-  //     ...filters,
-  //   },
-  // });
-  console.log(filters);
-  const { make, minPrice, maxPrice } = filters;
-
-  const { data, error, refresh } = await useAsyncData(
-    "test",
-    () =>
-      $fetch(`/api/cars/${city}`, {
-        params: {
-          ...filters,
-        },
-      }),
-    {
-      watch: [make, minPrice, maxPrice],
-    }
-  );
+  const { data, error, refresh } = await useFetch(`/api/cars/${city}`, {
+    params: {
+      ...filters,
+    },
+  });
 
   if (error.value) {
     throw createError({
