fe51c4f6020e51535a3de27491c5b53e29e0ac8a
Ashley Shang <utshang216@gmail.com>
2024-03-16 21:37:06 +0800

    Message：新增 message get 請求

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="rounded shadow mt-20">
-    <MessageCard />
-    <MessageCard />
-    <MessageCard />
-    <MessageCard />
+  <div v-if="messages.length > 0" class="rounded shadow mt-20">
+    <MessageCard
+      v-for="message in messages"
+      :key="message.id"
+      :message="message"
+    />
   </div>
+  <div v-else>There is no message.</div>
 </template>
 
 <script setup>
@@ -12,4 +14,10 @@ definePageMeta({
   layout: "custom",
   middleware: ["auth"],
 });
+
+const route = useRoute();
+
+const { data: messages } = useFetch(
+  `/api/car/listings/${route.params.id}/message`
+);
 </script>
